915c34d7a0c2bbda2e874c55121e9cbcf4a00727
Steve Aldworth <s.aldworth@tvdsb.ca>
2023-05-03 14:14:16 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Drawing_Bitmap_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgPlate = new System.Windows.Forms.PictureBox();
+            this.btnAddCorn = new System.Windows.Forms.Button();
+            this.btnAddCabbage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgPlate
+            // 
+            this.imgPlate.Image = global::Drawing_Bitmap_Test.Properties.Resources.essential_dinner_plate;
+            this.imgPlate.Location = new System.Drawing.Point(182, 45);
+            this.imgPlate.Name = "imgPlate";
+            this.imgPlate.Size = new System.Drawing.Size(383, 355);
+            this.imgPlate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgPlate.TabIndex = 0;
+            this.imgPlate.TabStop = false;
+            this.imgPlate.Paint += new System.Windows.Forms.PaintEventHandler(this.imgPlate_Paint);
+            // 
+            // btnAddCorn
+            // 
+            this.btnAddCorn.Location = new System.Drawing.Point(12, 12);
+            this.btnAddCorn.Name = "btnAddCorn";
+            this.btnAddCorn.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCorn.TabIndex = 1;
+            this.btnAddCorn.Text = "Corn";
+            this.btnAddCorn.UseVisualStyleBackColor = true;
+            this.btnAddCorn.Click += new System.EventHandler(this.btnAddCorn_Click);
+            // 
+            // btnAddCabbage
+            // 
+            this.btnAddCabbage.Location = new System.Drawing.Point(12, 45);
+            this.btnAddCabbage.Name = "btnAddCabbage";
+            this.btnAddCabbage.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCabbage.TabIndex = 2;
+            this.btnAddCabbage.Text = "Cabbage";
+            this.btnAddCabbage.UseVisualStyleBackColor = true;
+            this.btnAddCabbage.Click += new System.EventHandler(this.btnAddCabbage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddCabbage);
+            this.Controls.Add(this.btnAddCorn);
+            this.Controls.Add(this.imgPlate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlate)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgPlate;
+        private System.Windows.Forms.Button btnAddCorn;
+        private System.Windows.Forms.Button btnAddCabbage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using Drawing_Bitmap_Test.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drawing_Bitmap_Test
+{
+    public partial class Form1 : Form
+    {
+        Bitmap corn, plate, cabbage;
+      
+        // Stores the toppings that are 'on/in' the dish and need to be drawn
+        List<Bitmap> toppings; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            toppings = new List<Bitmap>();
+            corn = new Bitmap(Resources.corn3);
+            cabbage = new Bitmap(Resources.cabbage3);
+            plate = new Bitmap(Resources.essential_dinner_plate);
+            plate = new Bitmap(plate, new Size(imgPlate.Width, imgPlate.Height));
+            
+            
+            
+        
+        }
+
+        private void btnAddCorn_Click(object sender, EventArgs e)
+        {
+            if (toppings.Contains(corn))// User removes corn
+                toppings.Remove(corn);
+            else
+                toppings.Add(corn);  // User adds corn
+
+            this.Refresh(); // Calls the Paint() method which re-draws the image on the Picturebox
+        }
+
+
+        // This is called when Windows Forms decides that imgPain needs to be redrawn
+        private void imgPlate_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+            canvas.Clear(Color.White);  // Clears the Picturebox
+            canvas.DrawImage(plate, 0, 0); // Draws the plate
+            foreach (Bitmap topping in toppings) // Loops through all of the toppings that have been added and draws them
+            {
+                canvas.DrawImage(topping, 0, 0);
+            }
+        }
+
+        private void btnAddCabbage_Click(object sender, EventArgs e)
+        {
+            if (toppings.Contains(cabbage)) // User removes cabbage
+                toppings.Remove(cabbage);
+            else
+                toppings.Add(cabbage);  // User adds cabbage
+            
+            this.Refresh(); // Calls the Paint() method which re-draws the image on the Picturebox
+        }
+
+        
+    }
+}
